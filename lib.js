dd5e23ec006d3ac354ae31d3d8b7cd7405ec7eb6
Rodrigo Cabaço <42916568+RodrigoCabaco@users.noreply.github.com>
2024-06-08 15:29:41 +0100

    Update lib.js

--- lib.js
+++ lib.js
@@ -60,7 +60,7 @@ export async function performSwap(swapResponse, keypair, connexion, amount, toke
     return txid.toString();
 }
 
-const OPTIMIZED = 0.01;
+const OPTIMIZED = 10;
 
 const DEFAULT_OPTIONS = {
     sendOptions: {skipPreflight: true},
